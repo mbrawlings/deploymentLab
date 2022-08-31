828f19215015f495bbc21447e43a35a8e659d904
Matthew Rawlings <mbrawlings11@gmail.com>
2022-08-31 14:53:00 -0600

    2nd commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
 const express = require("express")
+const path = require("path")
+
 const app = express()
 
+//endpoints
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "..client/index.html"))
+})
+
+
+const port = process.env.PORT || 4011
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
