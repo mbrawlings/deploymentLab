e58e67a746ff525851c08d5b2f3b21a4d8c221c7
Matthew Rawlings <mbrawlings11@gmail.com>
2022-08-31 15:21:56 -0600

    added slash

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express()
 //endpoints
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "..client/index.html"))
+    res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
 
